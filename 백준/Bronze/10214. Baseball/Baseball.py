99de298e10bf57ce734c6a069de41a8212683cf9
wonwoo Kim <39664609+unul09@users.noreply.github.com>
2025-03-02 13:36:26 +0900

    [Bronze III] Title: Baseball, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10214. Baseball/Baseball.py
@@ -0,0 +1,15 @@
+t = int(input())
+
+for _ in range(t):
+    y_score = k_score = 0
+    for _ in range(9):
+        y, k = map(int, input().split())
+        y_score += y
+        k_score += k
+
+    if y_score > k_score:
+        print("Yonsei")
+    elif y_score < k_score:
+        print("Korea")
+    else:
+        print("Draw")
