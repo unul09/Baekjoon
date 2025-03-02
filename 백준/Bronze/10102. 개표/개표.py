47304f73fd70453194cea418a97f5b94ff11b179
wonwoo Kim <39664609+unul09@users.noreply.github.com>
2025-03-02 12:25:12 +0900

    [Bronze III] Title: 개표, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10102. 개표/개표.py
@@ -0,0 +1,9 @@
+v = int(input())
+votes = list(input())
+
+if votes.count("A") > votes.count("B"):
+    print("A")
+elif votes.count("A") < votes.count("B"):
+    print("B")
+else:
+    print("Tie")
