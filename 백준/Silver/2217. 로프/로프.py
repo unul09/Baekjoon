f6706aafc533cd1d8bb7e8cb44b5a49665da60e4
wonwoo Kim <39664609+unul09@users.noreply.github.com>
2025-03-02 17:19:56 +0900

    [Silver IV] Title: 로프, Time: 148 ms, Memory: 34080 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.py
@@ -0,0 +1,13 @@
+import sys
+data = []
+N = int(sys.stdin.readline())
+for i in range(N):
+    data.append(int(sys.stdin.readline()))
+
+data.sort()
+max = len(data) * data[0]
+for i in range(N):
+    if data[i] * (N - i) > max:
+        max = data[i] * (N - i)
+
+print(max)
