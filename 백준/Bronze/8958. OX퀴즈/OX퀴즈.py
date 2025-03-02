af5e40a3bcfd5960d6d7f87acdb8af36cecd9c72
wonwoo Kim <39664609+unul09@users.noreply.github.com>
2025-03-02 12:59:38 +0900

    [Bronze II] Title: OX퀴즈, Time: 124 ms, Memory: 37108 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -0,0 +1,14 @@
+import re
+
+t = int(input())
+
+for _ in range(t):
+    score = 0
+
+    result = input().split("X")
+    result = [s for s in result if s]
+
+    for Os in result:
+        score += len(Os)*(len(Os)+1)//2
+
+    print(score)
